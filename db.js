1b6803788dd7ff35ee926655dd674f692f431df5
pankajtawarey <pankajtaw65@gmail.com>
2025-01-12 19:31:27 +0530

    second commit

--- db.js
+++ db.js
@@ -4,9 +4,9 @@ const mongoose=require('mongoose');
 
 
 // const url='mongodb://localhost:27017/Hotal';
-// const url='mongodb+srv://codewebsite143:pankaj123@cluster0.yjdsc.mongodb.net/';
-// const url=process.env.MONGODB_URL;
-const url=process.env.LOCAL_HOST;
+// const url='mongodb+srv://codewebsite143:12345@cluster0.yjdsc.mongodb.net/';
+const url=process.env.MONGODB_URL;
+// const url=process.env.LOCAL_HOST;
 mongoose.connect(url);
 
 const db=mongoose.connection
